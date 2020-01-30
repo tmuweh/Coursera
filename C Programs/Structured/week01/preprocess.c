d0e6356344858b0797db41c32dd7217530a78dd3
Tangue Muweh <atmuweh@gmail.com>
2020-01-31 00:59:04 +0100

    print list of random gen for ages of teens between 25 and 65

--- C Programs/Structured/week01/preprocess.c
+++ C Programs/Structured/week01/preprocess.c
@@ -3,10 +3,41 @@
 	V1.0
 */
 
-#include <include.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+#define MAX_AGE 65
+#define MIN_AGE 25
+
+#define POPULATION 100
+#define RANGE 25
+
+#define AGE_OVER rand()%RANGE
+#define AGE AGE_OVER + MIN_AGE
+
+#define FILL for (int i = 0; i < POPULATION; ++i)\
+					data[i] = AGE
+
+void print_data(int d[], int size)
+{
+	int i;
+	printf("\n\n");
+	for (i = 0; i < size; ++i)
+	{
+		printf("%d\t", d[i]);
+		if ( (i+1) % 10 == 0) printf("\n");
+	}
+}
 
 int main()
 {
-	/* code */
+	int i;
+	int data[POPULATION];
+	srand(time(0));
+	FILL;
+	print_data(data, POPULATION);
+	printf("\n\n");
 	return 0;
 }
